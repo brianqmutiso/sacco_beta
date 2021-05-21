bd403d17060f54230efd540c1e47449c0000ac80
Brian Mutiso <brianqmutiso.com>
2021-05-21 05:15:40 -0700

    update

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('mvalidates', 'ClientController@mpesa_combined');
+
+Route::post('mconfirms', 'ClientController@mpesa_combined');
