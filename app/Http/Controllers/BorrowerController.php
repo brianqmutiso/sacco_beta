b200f5dfb2d92bc87e6c7f348d8a05b8ec961597
Brian Mutiso <brianqmutiso.com>
2021-05-28 22:33:58 -0700

    update

--- app/Http/Controllers/BorrowerController.php
+++ app/Http/Controllers/BorrowerController.php
@@ -294,7 +294,7 @@ class BorrowerController extends Controller
             }
         }
         $borrower->save();
-        $message="Dear ".$request->last_name.", Welcome to SHAKIVAN ENTERPRISE INVESTMENT your Account number is".$request->id_no.". May God fulfill your dreams";
+        $message="Dear ".$request->last_name.", Welcome to SHAKIVAN ENTERPRISE INVESTMENT your Account number is".$request->unique_number.". May God fulfill your dreams";
           new SendSMS($request->mobile,$message);
 
         $custom_fields = CustomField::where('category', 'borrowers')->get();
