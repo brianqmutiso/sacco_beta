a9443a8b2f122ea536a907cd9ff986271ce200e5
Brian Mutiso <brianqmutiso.com>
2021-06-11 06:33:20 -0700

    update

--- app/Helpers/SendSMS.php
+++ app/Helpers/SendSMS.php
@@ -13,31 +13,31 @@ class SendSMS
     {
 
 
-$apiKey   ="30ef9bf5ba6aaafffcf0afb054c3f106425b9d9e6eb98c88cf47b6728aa38452";
-$username="passsasa";
-$AT       = new AfricasTalking($username, $apiKey);
+// $apiKey   ="30ef9bf5ba6aaafffcf0afb054c3f106425b9d9e6eb98c88cf47b6728aa38452";
+// $username="passsasa";
+// $AT       = new AfricasTalking($username, $apiKey);
+// // $sms      = $AT->sms();
+
+
+// $pattern = "/^(0)\d{9}$/";
+// $pattern1 = "/^(254)\d{9}$/";
+// $pattern2 = "/^(\+254)\d{9}$/";
+// if (preg_match($pattern, $mobileInput)) {
+//   # code...
+//   $mobile="+254".substr($mobileInput,1);
+// }
+// else if(preg_match($pattern2, $mobileInput)){
+// $mobile=$mobileInput;
+// }
+// else if(preg_match($pattern1, $mobileInput)){
+// $mobile="+".$mobileInput;
+// }
 // $sms      = $AT->sms();
-
-
-$pattern = "/^(0)\d{9}$/";
-$pattern1 = "/^(254)\d{9}$/";
-$pattern2 = "/^(\+254)\d{9}$/";
-if (preg_match($pattern, $mobileInput)) {
-  # code...
-  $mobile="+254".substr($mobileInput,1);
-}
-else if(preg_match($pattern2, $mobileInput)){
-$mobile=$mobileInput;
-}
-else if(preg_match($pattern1, $mobileInput)){
-$mobile="+".$mobileInput;
-}
-$sms      = $AT->sms();
-$result   = $sms->send([
-    'to'      => $mobile,
-    'message' => $message
-]);
-\Log::info($result);
+// $result   = $sms->send([
+//     'to'      => $mobile,
+//     'message' => $message
+// ]);
+// \Log::info($result);
       
     }
 }
